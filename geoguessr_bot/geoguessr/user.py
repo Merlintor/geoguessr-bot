18f3f4384655668f5d88a8cc76da4923c25dad13
Merlintor <merlinfuchs2001@gmail.com>
2019-08-14 02:25:19 +0200

    Add login and register functionality

--- geoguessr_bot/geoguessr/user.py
+++ geoguessr_bot/geoguessr/user.py
@@ -8,8 +8,20 @@ class User:
         self.session = requests.Session()
         self.session.cookies.set(name="_ncfa", value=token)
 
-    def check_token(self):
-        return True
+    @staticmethod
+    def register(cls, email):
+        resp = requests.post("https://geoguessr.com/api/v3/accounts/signup", data={
+            "email": email
+        })
+        return cls(resp.cookies.get("_ncfa"))
+
+    @staticmethod
+    def login(cls, email, password):
+        resp = requests.post("https://geoguessr.com/api/v3/accounts/signin", data={
+            "email": email,
+            "password": password
+        })
+        return cls(resp.cookies.get("_ncfa"))
 
     def create_game(self, *args, **kwargs):
         return Game(user=self, *args, **kwargs)
