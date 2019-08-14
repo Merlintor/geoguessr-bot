0e256090cd1657d34ffa1d2f40cd562771ab2ee8
Merlintor <merlinfuchs2001@gmail.com>
2019-08-14 02:13:11 +0200

    Read token and maps from file

--- geoguessr_bot/menus/automated.py
+++ geoguessr_bot/menus/automated.py
@@ -1,19 +1,20 @@
 from menu import Menu
 import random
 import time
-from question import Question, OptionQuestion, IntegerQuestion, FloatQuestion
+from question import OptionQuestion, IntegerQuestion, FloatQuestion
 
 
-class MapsQuestion(Question):
+class WaitQuestion(OptionQuestion):
     def process_response(self, response, menu):
+        wait = super().process_response(response, menu)
         user = menu.find_result("user")
         count = menu.find_result("count")
         accuracy = menu.find_result("accuracy")
-        wait = menu.find_result("wait")
-        maps = response.replace(" ", "").split(",")
+        maps = open("maps.txt").read().replace(" ", "").split(",")
 
         for i in range(count):
-            game = user.create_game(map=random.choice(maps))
+            map = random.choice(maps)
+            game = user.create_game(map=map)
             for _ in range(game.roundCount):
                 pos = game.get_position()
                 lat_range = (pos["lat"] - pos["lat"] * (1 - accuracy), pos["lat"] + pos["lat"] * (1 - accuracy))
@@ -22,7 +23,12 @@ class MapsQuestion(Question):
                 game.answer(random.uniform(*lat_range), random.uniform(*lng_range))
 
             score = game.get_total_score()
-            print("\n%s. game is over. You scored %s points (%s percent)" % (i + 1, score["amount"], score["percentage"]))
+            print("\n%s. game is over. You scored %s points (%s percent) on %s" % (
+                i + 1,
+                score["amount"],
+                score["percentage"],
+                map
+            ))
 
             if wait:
                 time.sleep(1)
@@ -33,13 +39,9 @@ class AutomatedMenu(Menu):
         super().__init__(questions=[
             IntegerQuestion("count", "How many games should the bot play?"),
             FloatQuestion("accuracy", "Define the accuracy (0 - 1)"),
-            OptionQuestion(
+            WaitQuestion(
                 "wait",
                 "Should the bot wait a little bit between each game? (recommended)",
                 options={"yes": True, "no": False}
-            ),
-            MapsQuestion(
-                "game",
-                "Which maps do you want to play (e.g. 'world, usa, european-union, famous-places, uk, netherlands')"
             )
         ], parent=parent)
--- geoguessr_bot/menus/main.py
+++ geoguessr_bot/menus/main.py
@@ -1,25 +1,15 @@
 from menu import Menu
-from question import Question, OptionQuestion
-from exceptions import IllegalResponse
+from question import OptionQuestion, DirectResponse
 
 from geoguessr import User
 from .automated import AutomatedMenu
 from .manual import ManualMenu
 
 
-class TokenQuestion(Question):
-    def process_response(self, response, menu):
-        user = User(response)
-        if not user.check_token():
-            raise IllegalResponse("Invalid token given")
-
-        return user
-
-
 class MainMenu(Menu):
     def __init__(self):
         super().__init__(questions=[
-            TokenQuestion("user", "Please input your user token:"),
+            DirectResponse("user", User(open("token.txt.").read().strip())),
             OptionQuestion("mode", "Which mode do you want to use?", options={
                 "manual": ManualMenu,
                 "automated": AutomatedMenu
